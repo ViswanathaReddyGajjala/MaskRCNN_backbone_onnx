726062434158d028860f8af9b4f0d253f6f1919e
Viswanatha Reddy <viswanatha.g15@iiits.in>
2020-02-05 02:54:01 +0530

    Update check_onnx_model.py

--- check_onnx_model.py
+++ check_onnx_model.py
@@ -21,13 +21,13 @@ prepared_backend = onnx_caffe2_backend.prepare(model)
 W = {model.graph.input[0].name: x.data.numpy()}
 
 # Run the Caffe2 net:
-c2_out = prepared_backend.run(W)
+onnx_out = prepared_backend.run(W)
 
 # Verify the numerical correctness upto 3 decimal places
 
 for i in range(len(torch_out)):
-    print (torch_out[i].shape, c2_out[i].shape)
-    np.testing.assert_almost_equal(to_numpy(torch_out[i]), c2_out[i], decimal=3)
+    print (torch_out[i].shape, onnx_out[i].shape)
+    np.testing.assert_almost_equal(to_numpy(torch_out[i]), onnx_out[i], decimal=3)
     #np.testing.assert_almost_equal(to_numpy(torch_out[i]).reshape(c, h, w), c2_out[i], decimal=3)
 print("Exported model has been executed on Caffe2 backend, and the result looks good!")
 
